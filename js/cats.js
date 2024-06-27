e16699395cb37c55d80086d846fc04168a85283d
sairessy <sairessy@gmail.com>
2024-06-27 13:24:29 -0700

    Better

--- js/cats.js
+++ js/cats.js
@@ -19,7 +19,7 @@ facts = getFacts();
 let r = Math.round(Math.random() * facts.all.length);
 $(".intro p").text(facts.all[r].text);
 $(".intro p").append(
-  `<br>- <span style="color: ${myColor}; font-size: 16px;">${facts.all[r].user.name.first} ${facts.all[r].user.name.last}<span>`
+  `<br>~ <span style="color: ${myColor}; font-size: 16px;">${facts.all[r].user.name.first} ${facts.all[r].user.name.last}<span>`
 );
 
 setInterval(() => {
